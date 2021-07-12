745dfc6a8664346f694b2b96a1f94d7781285617
smallpence <smallpence.uk@gmail.com>
2021-07-12 16:07:05 +0100

    contents are printed

--- yelloweditor/src/main.rs
+++ yelloweditor/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{Write, BufReader, Read};
+use std::io::Result;
+
+fn main() -> Result<()> {
+    let f = File::open("../yellow.gbc")?;
+    let mut reader = BufReader::new(f);
+    let mut buff:Vec<u8> = Vec::new();
+
+    reader.read_to_end(&mut buff);
+
+    let mut i:u8 = 0;
+    for num in buff {
+        print!("{} ", num);
+        i+=1;
+        if i == 8 { println!(""); i = 0; }
+    }
+
+    Ok(())
 }
