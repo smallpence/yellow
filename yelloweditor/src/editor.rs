a81530bc501ac4a5c65b7bb5e23fb64c0a3d774c
smallpence <smallpence.uk@gmail.com>
2021-07-13 16:11:34 +0100

    added up & down commands, refactored printing rom to a function

--- yelloweditor/src/editor.rs
+++ yelloweditor/src/editor.rs
@@ -26,57 +26,71 @@ impl<'a> ROMEditor<'a> {
         self.init();
 
         let mut command = String::new();
-        let rom_size_length = format!("{:x}",self.rom.size()).len();
 
-        // each branch MUST print 8 lines
-        for i in 0..10 {
+        loop {
+            if command.eq("quit") { break }
+
             self.clear();
-            self.println(&format!("{}",rom_size_length));
 
             self.println(&format!("POKEMON YELLOW ROM EDITOR"));
 
-            match command.to_lowercase().trim() {
-                "help" => {
-                    self.println(&String::from("hello world!"));
+            match command.as_str() {
+                "d" => {
+                    self.line += PRINT_INTERVAL * ROM_LINE_COUNT as u32;
+                    self.print_rom();
                 }
+
+                "u" => {
+                    self.line -= PRINT_INTERVAL * ROM_LINE_COUNT as u32;
+                    self.print_rom();
+                }
+
                 "size" => {
                     self.println(&format!("rom size: {:#x}", self.rom.size()))
                 }
+
                 _ => {
-                    // begin printing in bold
-                    self.print(&String::from("\x1B[1m"));
-
-                    // print the hex from 0-PRINT_INTERVAL as well as a gap
-                    let t: String = (0..PRINT_INTERVAL).map(|x| format!("{:02x} ",x)).collect();
-                    self.println(&format!("{} {}"," ".repeat(rom_size_length), t));
-
-                    // no more bold
-                    self.print(&String::from("\x1B[0m"));
-
-                    let mut rom_iter = self.rom.iterator_from(self.line);
-                    // current line on the display
-                    let mut display_line = self.line;
-                    for _ in 0..ROM_LINE_COUNT {
-                        let mut line_hex = String::new();
-                        for _ in 0..PRINT_INTERVAL {
-                            let next = rom_iter.next().unwrap();
-                            let next = match self.rom.dict.get(*next) {
-                                Some(chars) => format!("{:} ", chars),
-                                None               => format!("{:02x} ", next).to_uppercase()
-                            };
-                            line_hex.push_str(next.as_str());
-                        }
-                        self.println(&format!("{:01$x} {2}", display_line, rom_size_length, line_hex));
-                        display_line += PRINT_INTERVAL;
-                    }
+                    self.print_rom();
                 }
             }
 
             self.flush();
-            command = self.command_prompt()?;
+
+            command = self.command_prompt().trim().to_lowercase();
         }
 
         Ok(())
+    }
+
+    fn print_rom(&mut self) {
+        let rom_size_length = format!("{:x}",self.rom.size()).len();
+
+        // begin printing in bold
+        self.print(&String::from("\x1B[1m"));
+
+        // print the hex from 0-PRINT_INTERVAL as well as a gap
+        let t: String = (0..PRINT_INTERVAL).map(|x| format!("{:02x} ", x)).collect();
+        self.println(&format!("{} {}", " ".repeat(rom_size_length), t));
+
+        // no more bold
+        self.print(&String::from("\x1B[0m"));
+
+        let mut rom_iter = self.rom.iterator_from(self.line);
+        // current line on the display
+        let mut display_line = self.line;
+        for _ in 0..ROM_LINE_COUNT {
+            let mut line_hex = String::new();
+            for _ in 0..PRINT_INTERVAL {
+                let next = rom_iter.next().unwrap();
+                let next = match self.rom.dict.get(*next) {
+                    Some(chars) => format!("{:} ", chars),
+                    None => format!("{:02x} ", next).to_uppercase()
+                };
+                line_hex.push_str(next.as_str());
+            }
+            self.println(&format!("{:01$x} {2}", display_line, rom_size_length, line_hex));
+            display_line += PRINT_INTERVAL;
+        }
     }
 
     // wrapper for print as well for consistency
@@ -113,14 +127,14 @@ impl<'a> ROMEditor<'a> {
     }
 
     // 1 line
-    fn command_prompt(&mut self) -> Result<String> {
+    fn command_prompt(&mut self) -> String {
         print!(">>> ");
         self.flush();
         self.printed_count += 1;
 
         let mut s = String::new();
-        stdin().read_line(&mut s)?;
-        Ok(s)
+        stdin().read_line(&mut s).unwrap();
+        s
     }
 
     #[inline]
